a73f61adab42b5268796683c642b803b90fa0089
Brian Sohn <kolpp15@gmail.com>
2021-07-29 23:37:46 -0700

    pair programming - word search in 2D array

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,32 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function(matrix) {
+  let result = [];
+  
+  for (let i = 0; i < matrix[0].length; i++) {
+    const tempMatrix = [];
+    for (let j = 0; j < matrix.length; j++) {
+      tempMatrix.push(matrix[j][i]);
     }
-}
+    result.push(tempMatrix);
+  }
+  return result;
+};
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+
+  const verticalJoin = transpose(letters).map(ls => ls.join(''));
+  for (const m of verticalJoin) {
+    if (m.includes(word)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+module.exports = wordSearch;
